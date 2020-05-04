c20a2e11db288685dae02f8df73e6127038d9d4e
Tyler Simeone <tyler.simeone@gmail.com>
2020-05-04 13:33:31 -0500

    added some sample library and book data

---
+++ libraryproject/sample_data.sql
@@ -0,0 +1,14 @@
+INSERT INTO libraryapp_library
+(name, address)
+VALUES
+('Bellview Library', '500 Main Street');
+
+INSERT INTO libraryapp_book
+(title, isbn, author, year_published, publisher, librarian_id, location_id)
+VALUES
+('Lamb', '59359509', 'Christopher Moore', 2004, "some publishing co.", 1, 1);
+
+INSERT INTO libraryapp_book
+(title, isbn, author, year_published, publisher, librarian_id, location_id)
+VALUES
+('Taiko', '0472809124893', 'Eiji Yoshikawa', 2001,"some publishing co.", 1, 1);
