9040d691d194dfbc4a5f0c9c95feffcd96273d48
Tyler Simeone <tyler.simeone@gmail.com>
2020-05-05 10:33:55 -0500

    added logout redirect path, and login_required decorator to all views so user must be logged-in to view pages.

--- libraryproject/libraryapp/views/books/list.py
+++ libraryproject/libraryapp/views/books/list.py
@@ -1,9 +1,10 @@
 import sqlite3
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from libraryapp.models import Book
 from ..connection import Connection
 
-
+@login_required
 def book_list(request):
     if request.method == 'GET':
         with sqlite3.connect(Connection.db_path) as conn:
