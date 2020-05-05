9de257e956f278cc67c8a35de3df972ce4b0b0b0
Tyler Simeone <tyler.simeone@gmail.com>
2020-05-05 10:08:24 -0500

    created login template and url pattern

--- libraryproject/libraryapp/urls.py
+++ libraryproject/libraryapp/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import include, path
 from .views import *
 
 app_name = "libraryapp"
@@ -8,4 +8,5 @@ urlpatterns = [
     path('books/', book_list, name='books'),
     path('librarians/', list_librarians, name='librarians'),
     path('libraries/', library_list, name='libraries'),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
