17dd5c8a8d4090643b280e42a055acf179b8bb14
Tyler Simeone <tyler.simeone@gmail.com>
2020-05-06 11:23:33 -0500

    added some comments on book form.py and list.py for clarity on what's happening

--- libraryproject/libraryapp/views/__init__.py
+++ libraryproject/libraryapp/views/__init__.py
@@ -2,5 +2,6 @@ from .books.list import book_list
 from .books.form import book_form
 from .librarians.list import list_librarians
 from .libraries.list import library_list
+from .libraries.form import library_form
 from .home import home
 from .auth.logout import logout_user
--- libraryproject/libraryapp/views/books/form.py
+++ libraryproject/libraryapp/views/books/form.py
@@ -6,8 +6,9 @@ from libraryapp.models import Library
 from libraryapp.models import model_factory
 from ..connection import Connection
 
-# only runs when 'book/form' url pattern is ran 
-# and the vals it returns go into book_form
+# The below function is only to get a list of libraries for
+# the select menu on the add book form, for which library you 
+# would like to add the book.
 def get_libraries():
     with sqlite3.connect(Connection.db_path) as conn:
         conn.row_factory = model_factory(Library)
@@ -23,6 +24,8 @@ def get_libraries():
 
         return db_cursor.fetchall()
 
+# Below is the function to get and display the form template to 
+# the user (with all the libraries for that last input field as well).
 @login_required
 def book_form(request):
     if request.method == 'GET':
