d28316ad1131fc80c58c73871db43ea0bc010e17
Tyler Simeone <tyler.simeone@gmail.com>
2020-05-07 14:50:23 -0500

    extended shared base template to all other templates, and removed all common elements with base template from every other template.

--- libraryproject/libraryapp/urls.py
+++ libraryproject/libraryapp/urls.py
@@ -6,14 +6,14 @@ app_name = "libraryapp"
 urlpatterns = [
     path('', home, name='home'),
     path('books/', book_list, name='books'),
+    path('book/form/', book_form, name='book_form'),
     path('books/<int:book_id>/', book_details, name='book'),
     path('books/<int:book_id>/form/', book_edit_form, name='book_edit_form'),
     path('librarians/', list_librarians, name='librarians'),
     path('librarians/<int:librarian_id>/', librarian_details, name='librarian'),
     path('libraries/', library_list, name='libraries'),
+    path('library/form/', library_form, name='library_form'),
     path('libraries/<int:library_id>/', library_details, name='library'),
     path('accounts/', include('django.contrib.auth.urls')),
     path('logout/', logout_user, name='logout'),
-    path('book/form', book_form, name='book_form'),
-    path('library/form', library_form, name='library_form'),
 ]
