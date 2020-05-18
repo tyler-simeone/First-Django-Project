cbbfc6be0f70ab49b6c61af59ac9141ea1480aa9
Tyler Simeone <tyler.simeone@gmail.com>
2020-05-18 14:45:39 -0500

    added ORM for librarian details.py view

--- libraryproject/libraryapp/views/librarians/details.py
+++ libraryproject/libraryapp/views/librarians/details.py
@@ -2,30 +2,35 @@ import sqlite3
 from django.urls import reverse
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from libraryapp.models import Librarian
+from libraryapp.models import Librarian, Library
 from libraryapp.models import model_factory
 from ..connection import Connection
 
 
 def get_librarian(librarian_id):
-    with sqlite3.connect(Connection.db_path) as conn:
-        conn.row_factory = model_factory(Librarian)
-        db_cursor = conn.cursor()
+    librarian = Librarian.objects.get(pk=librarian_id)
+    librarian.library = Library.objects.get(pk=librarian.location_id)
 
-        db_cursor.execute("""
-        SELECT
-            l.id,
-            l.location_id,
-            l.user_id,
-            u.first_name,
-            lb.name
-        FROM libraryapp_librarian l
-        LEFT JOIN auth_user u ON l.user_id = u.id
-        LEFT JOIN libraryapp_library lb ON l.location_id = lb.id
-        WHERE l.id = ?
-        """, (librarian_id,))
+    return librarian
 
-        return db_cursor.fetchone()
+    # with sqlite3.connect(Connection.db_path) as conn:
+    #     conn.row_factory = model_factory(Librarian)
+    #     db_cursor = conn.cursor()
+
+    #     db_cursor.execute("""
+    #     SELECT
+    #         l.id,
+    #         l.location_id,
+    #         l.user_id,
+    #         u.first_name,
+    #         lb.name
+    #     FROM libraryapp_librarian l
+    #     LEFT JOIN auth_user u ON l.user_id = u.id
+    #     LEFT JOIN libraryapp_library lb ON l.location_id = lb.id
+    #     WHERE l.id = ?
+    #     """, (librarian_id,))
+
+    #     return db_cursor.fetchone()
 
 @login_required
 def librarian_details(request, librarian_id):
